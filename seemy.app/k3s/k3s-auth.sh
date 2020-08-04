6e18c3109c6e6f0e904cc37c45f51629f1996812
Brian Romanko <hello@bromanko.com>
2020-08-04 16:52:36 -0500

    Fix env var name for errors

--- seemy.app/k3s/k3s-auth.sh
+++ seemy.app/k3s/k3s-auth.sh
@@ -3,7 +3,7 @@
 set -e
 
 K8S_OICD_CLIENT_ID="302508247932-5usfekhtdbkq4gkup22kraqbqq70gubt.apps.googleusercontent.com"
-[ -z "$K8S_OIDC_CLIENT_SECRET" ] && (echo "CLIENT_SECRET env variable must be set."; exit 1)
+[ -z "$K8S_OIDC_CLIENT_SECRET" ] && (echo "K8S_OIDC_CLIENT_SECRET env variable must be set."; exit 1)
 
 docker run --rm -it \
   -v "$HOME/.kube:/etc/kube" \
