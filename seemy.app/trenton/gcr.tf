dda6adf478f3d9fd9ddd1594df44fbb77c4f667a
Brian Romanko <hello@bromanko.com>
2020-08-04 16:52:53 -0500

    Add GCR RO service account

--- seemy.app/trenton/gcr.tf
+++ seemy.app/trenton/gcr.tf
@@ -4,3 +4,10 @@ module "gcr-rw" {
   project_id     = module.project.project_id
   account_id     = "trenton-gcr-rw"
 }
+
+module "gcr-ro-k3s" {
+  source         = "../terraform/modules/gcr_reader"
+  gcr_project_id = data.terraform_remote_state.images.outputs.project_id
+  project_id     = module.project.project_id
+  account_id     = "trenton-gcr-ro-k3s"
+}
