a52901fd38c138fc20126a8188abc8fd435bfeb1
Brian Romanko <hello@bromanko.com>
2020-08-07 12:36:52 -0500

    Add project_id

--- seemy.app/trenton/webhooks.tf
+++ seemy.app/trenton/webhooks.tf
@@ -1,4 +1,5 @@
 resource "google_project_service" "datastore" {
+  project = module.project.project_id
   disable_dependent_services = false
   disable_on_destroy = false
   service = "datastore.googleapis.com"
