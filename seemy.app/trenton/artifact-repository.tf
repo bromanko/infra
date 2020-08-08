790f3c01c3a7a5fb017f47eff1e922852f3a178c
Brian Romanko <hello@bromanko.com>
2020-08-08 15:48:07 -0500

    Add project_id

--- seemy.app/trenton/artifact-repository.tf
+++ seemy.app/trenton/artifact-repository.tf
@@ -1,5 +1,6 @@
 resource "google_artifact_registry_repository" "artifact_repository" {
   provider      = google-beta
+  project       = module.project.project_id
   location      = "us-central1"
   repository_id = "trenton"
   format        = "DOCKER"
