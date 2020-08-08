d728344b1c6a715195fef5c060fb5c95aa9b998f
Brian Romanko <hello@bromanko.com>
2020-08-08 16:05:46 -0500

    Remove container registry modules

--- terraform/modules/gcr_reader/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "service_account_key" {
-  value     = google_service_account_key.ro_key
-  sensitive = true
-}
--- terraform/modules/gcr_writer/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "service_account_key" {
-  value     = google_service_account_key.rw_key
-  sensitive = true
-}
