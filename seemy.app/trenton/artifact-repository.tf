0ec1a9f6aa276596db1697cb697c9978627374f7
Brian Romanko <hello@bromanko.com>
2020-08-08 15:51:44 -0500

    Enable artifactregistry API

--- seemy.app/trenton/artifact-repository.tf
+++ seemy.app/trenton/artifact-repository.tf
@@ -1,5 +1,14 @@
+resource "google_project_service" "artifact_repository" {
+  provider                   = google
+  project                    = module.project.project_id
+  disable_dependent_services = false
+  disable_on_destroy         = false
+  service                    = "artifactregistry.googleapis.com"
+}
+
 resource "google_artifact_registry_repository" "artifact_repository" {
   provider      = google-beta
+  depends_on    = [google_project_service.artifact_repository]
   project       = module.project.project_id
   location      = "us-central1"
   repository_id = "trenton"
