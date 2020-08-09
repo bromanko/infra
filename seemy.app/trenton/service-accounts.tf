9cb2ed58d2160abf457d6b9f1915e80d2ea3480a
Brian Romanko <hello@bromanko.com>
2020-08-08 20:47:26 -0500

    Allow CI to write to artifact registry

--- seemy.app/trenton/service-accounts.tf
+++ seemy.app/trenton/service-accounts.tf
@@ -24,6 +24,6 @@ resource "google_artifact_registry_repository_iam_member" "ci" {
   project    = module.project.project_id
   location   = google_artifact_registry_repository.artifact_repository.location
   repository = google_artifact_registry_repository.artifact_repository.name
-  role       = "roles/artifactregistry.reader"
+  role       = "roles/artifactregistry.writer"
   member     = "serviceAccount:${google_service_account.ci.email}"
 }
