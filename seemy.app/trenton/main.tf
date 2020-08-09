2cf6d55d3eaa60e607ef05a70dc0725e24eda2ea
Brian Romanko <hello@bromanko.com>
2020-08-08 21:40:48 -0500

    Remove container registry remote

--- seemy.app/trenton/main.tf
+++ seemy.app/trenton/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  backend "remote" {
-    organization = "bromanko"
-
-    workspaces {
-      name = "trenton"
-    }
-  }
-}
-
 data "terraform_remote_state" "images" {
   backend = "remote"
 
