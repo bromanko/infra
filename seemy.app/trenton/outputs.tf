8891bbbca005d657f9371f1a537007e3fbc0e05b
Brian Romanko <hello@bromanko.com>
2020-09-01 10:15:55 -0500

    Add web private key to outputs

--- seemy.app/trenton/outputs.tf
+++ seemy.app/trenton/outputs.tf
@@ -7,3 +7,8 @@ output "ci_service_account_key" {
   value     = google_service_account_key.ci_account_key.private_key
   sensitive = true
 }
+
+output "web_service_account_key" {
+  value = google_service_account_key.production_web_server.private_key
+  sensitive = true
+}
