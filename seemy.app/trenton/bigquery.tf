15d2ff86f14f7caaba2e95ab54c0bbcc6910a40b
Brian Romanko <hello@bromanko.com>
2020-10-30 09:12:19 -0500

    Allow webhooks to write to bucket

---
+++ seemy.app/trenton/bigquery.tf
@@ -0,0 +1,11 @@
+resource "google_project_service" "bigquery" {
+  provider                   = google
+  project                    = module.project.project_id
+  disable_dependent_services = false
+  disable_on_destroy         = false
+  service                    = "bigquery.googleapis.com"
+}
+
+resource "google_bigquery_dataset" "health" {
+  dataset_id = "health"
+}
--- seemy.app/trenton/storage.tf
+++ seemy.app/trenton/storage.tf
@@ -1,6 +1,11 @@
-resource "google_storage_bucket" "data" {
+resource "google_storage_bucket" "production_data" {
   project       = module.project.project_id
   name          = "trenton-production-data"
   force_destroy = true
 }
 
+resource "google_storage_bucket_access_control" "production_data_webhooks" {
+  bucket = google_storage_bucket.production_data.name
+  entity = "serviceAccount:${google_service_account.production_webhooks_server.email}"
+  role   = "OWNER"
+}
