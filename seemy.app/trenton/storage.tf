5bb38dbf651a8de66b01210e466a49c313028780
Brian Romanko <hello@bromanko.com>
2020-10-30 10:10:15 -0500

    Fix ACL

--- seemy.app/trenton/storage.tf
+++ seemy.app/trenton/storage.tf
@@ -7,5 +7,5 @@ resource "google_storage_bucket" "production_data" {
 resource "google_storage_bucket_iam_member" "production_data_webhooks" {
   bucket = google_storage_bucket.production_data.name
   member = "serviceAccount:${google_service_account.production_webhooks_server.email}"
-  role   = "roles/storage.objectWriter"
+  role   = "roles/storage.objectCreator"
 }
