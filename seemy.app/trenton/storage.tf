6f856011a95a51a53eb5cbf6659f1dfb3de34bf3
Brian Romanko <hello@bromanko.com>
2020-11-09 09:19:36 -0600

    Switch CI user to an objectAdmin on the test bucket

--- seemy.app/trenton/storage.tf
+++ seemy.app/trenton/storage.tf
@@ -7,7 +7,7 @@ resource "google_storage_bucket" "production_data" {
 resource "google_storage_bucket_iam_member" "production_data_webhooks" {
   bucket = google_storage_bucket.production_data.name
   member = "serviceAccount:${google_service_account.production_webhooks_server.email}"
-  role   = "roles/storage.objectCreator"
+  role   = "roles/storage.objectAdmin"
 }
 
 resource "google_storage_bucket" "test_data" {
